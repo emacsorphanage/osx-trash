0f1dc052d0a750b8c75f14530a4897f5d4324b4e
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-05-20 15:00:09 +0200

    Fix checkdoc warning

--- osx-trash.el
+++ osx-trash.el
@@ -29,7 +29,7 @@
 ;; `delete-by-moving-to-trash' do what you expect it to do.
 ;;
 ;; Emacs does not support the system trash of OS X.  `system-move-file-to-trash'
-;; is not defined. This library provides `osx-trash-move-file-to-trash' as an
+;; is not defined.  This library provides `osx-trash-move-file-to-trash' as an
 ;; implementation of `system-move-file-to-trash' for OS X.
 ;;
 ;; `osx-trash-move-file-to-trash' tries to use `trash' utility from
